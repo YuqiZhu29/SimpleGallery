f5f587c69f70bea78f4a0c9209b8091ad34af5dc
YuqiZhu29 <yzhu29@stevens.edu>
2018-07-05 23:31:35 -0700

    Pure Client

--- js/controller.js
+++ js/controller.js
@@ -1,18 +1,21 @@
 $(document).ready(function() {
 	//get random number from Gallery object;
-	var gallery = new Gallery(1000);
-	displayGallery(gallery);
+	var gallery = new Gallery(100, 100, 100);
+	generateGallery(gallery);
 	fetchThumbnails(gallery);
+
 });
 
 //display gallery on html page random number of placeholder
-function displayGallery(gallery) {
+function generateGallery(gallery) {
 	try{
 		var thumbnailsNumber = gallery.getNumber();
 		var arr = [];
 		for(var i = 0; i < thumbnailsNumber; i++) {
-			var $ph = $("<div id='ph" + (i+1) + "' class='placeHolder'><div class='content'><img></div></div>");
+			var uuid = gallery.generateUUID();
+			var $ph = $("<div id='" + uuid + "' class='placeHolder'><div class='content'><img></div></div>");
 			arr.push($ph);
+			gallery.placeHolders.push(new PlaceHolder(uuid));
 		}
 		$(".mainContainer").append(arr);
 		$(".placeHolder").height(gallery.getHeight());
@@ -20,8 +23,7 @@ function displayGallery(gallery) {
 	}catch(error) { 
 		console.log(error);
 	}
-	console.log("Created " + arr.length + " without thumbnails.");
-	return arr;
+	console.log("Created " + arr.length + " placeholders without thumbnails.");
 }
 
 function fetchThumbnails(gallery) {
@@ -33,13 +35,17 @@ function fetchThumbnails(gallery) {
 		dataType: "json",
 		success: function(data){
 			console.log("Fetch data success.");
-			for(var i = 0; i <= gallery.getNumber(); i++) {
-				$("#ph" + i + "> div > img").attr("src", data[i].url);
+			for(var i = 0; i < gallery.getNumber(); i++) {
+				var url = data[i].url;
+				gallery.getPlaceHolder(i).setUrl(url);
+				//gallery.getPlaceHolder(i).setBase64(url);
+				$("#" + gallery.getPlaceHolder(i).getUUID() + "> div > img").attr("src", url);
 			}
 			$("img").attr("alt", "");
 		},
 		error: function(XMLHtttpRequest, textStatus, errorThrown) {
-			alert("Error! code: " + XMLHtttpRequest.status);
+			console.log("Thumbnails load failed." + "Error code: " + XMLHtttpRequest.status);
+			$("img").attr("alt", "Load failed");
 		}
 	});
 }
--- model.js
+++ model.js
@@ -1,9 +1,11 @@
 class Gallery{
-	constructor(number) {
-		//get random number of thumbnails, ps: from 1 to number
-		this.number = Math.floor(Math.random() * number + 1);
-		this.width = 200;
-		this.height = 200;
+	constructor(upBound, width, height) {
+		//get random number of thumbnails, ps: from 1 to upBound
+		this.number = Math.floor(Math.random() * upBound + 1);
+		this.width = width;
+		this.height = height;
+		this.placeHolders = [];
+		console.log("Create Gallery.");
 	}
 	getNumber() {
 		return this.number;
@@ -14,16 +16,47 @@ class Gallery{
 	getHeight() {
 		return this.height;
 	}
+	getPlaceHolder(index) {
+		return this.placeHolders[index];
+	}
+	generateUUID(){
+	    var d = new Date().getTime();
+	    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	        var r = (d + Math.random()*16)%16 | 0;
+	        d = Math.floor(d/16);
+	        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+	    });
+	    return uuid;
+	};
 }
 
 class PlaceHolder{
-	constructor(id, height, width) {
-		this.width = width;
-		this.height = height;
-		this.id = id;
-		this.url = "/w3images/cars1.jpg";
+	constructor(uuid) {
+		this.uuid = uuid;
+	}
+	getUUID() {
+		return this.uuid;
 	}
 	setUrl(url) {
 		this.url = url;
+	}
+	getUrl() {
+		return this.url;
+	}
+	setBase64(url) {
+		var canvas = document.createElement("canvas");
+		var ctx = canvas.getContext("2d");
+		var img = new Image();
+		var base64 = '';//base64 d
+		img.crossOrigin = "Anonymous";
+		img.src = url;
+		img.onload = function(){
+		   ctx.drawImage(img,0,0);    
+		   base64 = canvas.toDataURL(); 
+		};
+		this.base64 = base64;
+	}
+	getBase64() {
+		return this.base64;
 	}
 }
